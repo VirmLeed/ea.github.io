68b7e4f75154166cc71ac8d35bfd1927f3c52d36
VirmLeed <58979393+VirmLeed@users.noreply.github.com>
2023-06-18 17:40:30 +0300

    много где немного исправлений и чуть-чуть помощи

---
+++ support.js
@@ -0,0 +1,11 @@
+let faqFolder = document.querySelector(".faqFolder");
+let faqName = document.querySelector(".faqName");
+let faqArrow = document.querySelector(".faqArrow");
+
+faqName.addEventListener('click', fold);
+
+function fold(){
+    faqFolder.classList.toggle('active');
+    faqArrow.classList.toggle('active');
+    console.log("active");
+}
