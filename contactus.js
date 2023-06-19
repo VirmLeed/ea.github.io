9d6403a80f808067b7a5147021e89bcd8eadae19
VirmLeed <58979393+VirmLeed@users.noreply.github.com>
2023-06-19 21:46:37 +0300

    теперь всё корректно обнуляется при отправке письм

--- contactus.js
+++ contactus.js
@@ -3,13 +3,13 @@ let truemail = document.getElementById("mailTest");
 let type = document.getElementById("messageType").value;
 let subject = document.getElementById("subject");
 let message = document.getElementById("message");
+let form = document.getElementById("formBox");
 let weirdlettercollection = "1BDE0A7C3F893B90A2D371DEE3602DB7BF40";
 let wrong = document.getElementById("wrong");
 let sent = document.getElementById("sent");
 
 function sendEmail(){
     if(!mail.value.localeCompare(truemail.value)){
-        reset(); 
         wrong.classList.remove('active');
         Email.send({
             SecureToken : "3b588c44-1ddf-466c-b8d2-207a7aeb752d",
@@ -19,8 +19,11 @@ function sendEmail(){
             Body : message.value + '\n // ' + truemail.value,
         }).then(/*message => alert(message)*/);
         sent.classList.add('active');
+        form.reset();
+        return false;
     }else{
         wrong.classList.add('active');
         sent.classList.remove('active');
+        return false;
     }
 }
